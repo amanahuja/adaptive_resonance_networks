a8beadec90bb9f2399dd426966fe2de704a59c45
Aman Ahuja <amanahuja@gmail.com>
2016-02-03 20:27:38 -0800

    placeholder for LAPACK

---
+++ LAPACK.py
@@ -0,0 +1,13 @@
+"""
+ART - LAPACK
+Adaptive Resonance Theory Neural Networks
+by Aman Ahuja | github.com/amanahuja | twitter: @amanqa
+
+"""
+
+
+class LAPART1:
+    """
+    Lateral Adaptive Priming Adaptive Resonance Theory architecture
+    """
+    pass
